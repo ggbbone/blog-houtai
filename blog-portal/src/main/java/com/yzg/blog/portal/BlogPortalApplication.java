f1fca0d113df79baae42fc6fa88c0178bec8cb5d
yangzg <yang847857204@gmail.com>
2019-12-17 16:09:31 +0800

    前台博客系统接口

---
+++ blog-portal/src/main/java/com/yzg/blog/portal/BlogPortalApplication.java
@@ -0,0 +1,11 @@
+package com.yzg.blog.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPortalApplication.class, args);
+    }
+}
